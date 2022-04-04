c984b676ba7bb6098262d7fa03f85c6782b690f1
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-04-04 21:56:10 +0900

    [feat] NormalList 생성

---
+++ src/components/NormalListComponent.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container" @blur="fnBlur()">
+    <div class="main">
+      <ul class="item__list">
+        <li class="item" v-for="(item, index) in itemList" :key="index" >
+          {{ item.name }}
+        </li>
+      </ul>
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'NormalList',
+  data() {
+    return {
+      itemList: [
+        {
+          name: '설정'
+        },
+        {
+          name: '잠금모드'
+        },
+        {
+          name: '로그아웃'
+        },
+        {
+          name: '종료'
+        }
+      ]
+    }
+  },
+  methods: {
+    fnBlur() {
+      console.log(blur);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  min-width: 140px;
+  max-width: 210px;
+  position: fixed;
+  z-index: 200;
+  border: 1px solid;
+
+  .main {
+    width: 100%;
+    background: white;
+    .item__list {
+      padding: 2px 0;
+      .item {
+        display: flex;
+        align-items: center;
+        font-size: 12px;
+        height: 25px;
+        margin: 0px 0;
+        padding: 5px 10px;
+        &:hover {
+          background: #F0F0F0;
+        }
+      }
+    }
+  }
+}
+</style>
