97a052ca0c27d66a090a40db738794c2ebba617d
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-04-10 20:01:00 +0900

    [feat] 화면 아무데나 클릭시 normalList 제거

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <RouterView />
+  <div class="container" @click="fnResetToggle()">
+    <RouterView />
+  </div>
+
 </template>
 
 <script>
 
 export default {
   name: 'App',
+  methods: {
+    fnResetToggle() {
+      this.$store.state.toggle.chatSort = false;
+      this.$store.state.toggle.setting = false;
+    }
+  }
 }
 </script>
 
