d2ff0f3debf393d5ca8e1145647b16efedef89ae
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-04-14 15:29:39 +0900

    [fix] friendsview scroll 기준 수정

--- src/views/FriendsView.vue
+++ src/views/FriendsView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="friendsview__container">
     <Nav selected="friends" />
     <div class="main">
       <header class="header">
@@ -17,58 +17,59 @@
           </span>
         </div>
       </header>
-
-      <section class="user__profile">
-        <div class="profile">
-          <div class="profile__image">
-            <img src="@/assets/kakaologo.png" alt="kakao.png">
-          </div>
-          <div class="profile__textarea">
-            <div class="profile__name">
-              강현대
+      <div class="contents">
+        <section class="user__profile">
+          <div class="profile">
+            <div class="profile__image">
+              <img src="@/assets/kakaologo.png" alt="kakao.png">
             </div>
-            <div class="profile__hello">
-              호멤메....
+            <div class="profile__textarea">
+              <div class="profile__name">
+                강현대
+              </div>
+              <div class="profile__hello">
+                호멤메....
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <section class="friends">
-        <div class="friends__title">
-          <span>친구 238</span>
-          <span>^</span>
-        </div>
-        <div class="friend__container">
-          <div class="friend">
-            <Profile />
+        </section>
+        <section class="friends">
+          <div class="friends__title">
+            <span>친구 238</span>
+            <span>^</span>
           </div>
-          <div class="friend">
-            <Profile />
+          <div class="friend__container">
+            <div class="friend">
+              <Profile />
+            </div>
+            <div class="friend">
+              <Profile />
+            </div>
+            <div class="friend">
+              <Profile />
+            </div>
+            <div class="friend">
+              <Profile />
+            </div>
+            <div class="friend">
+              <Profile />
+            </div>
+            <div class="friend">
+              <Profile />
+            </div>
+            <div class="friend">
+              <Profile />
+            </div>
+            <div class="friend">
+              <Profile />
+            </div>
+            <div class="friend">
+              <Profile />
+            </div>
           </div>
-          <div class="friend">
-            <Profile />
-          </div>
-          <div class="friend">
-            <Profile />
-          </div>
-          <div class="friend">
-            <Profile />
-          </div>
-          <div class="friend">
-            <Profile />
-          </div>
-          <div class="friend">
-            <Profile />
-          </div>
-          <div class="friend">
-            <Profile />
-          </div>
-          <div class="friend">
-            <Profile />
-          </div>
-        </div>
 
-      </section>
+        </section>
+      </div>
     </div>
   </div>
 </template>
@@ -87,92 +88,107 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.container {
+.friendsview__container {
   display: flex;
-}
-.main {
-  display: flex;
-  flex-direction: column;
-  margin-top: 40px;
-  overflow-x: hidden;
-  overflow-y: auto;
-  padding-left: 80px; /** Navigtion width에 따라 변경해야함 */
   width: 100vw;
-  .header {
+  height: 100vh;
+  overflow: hidden;
+  
+  .main {
     display: flex;
-    position: relative;
-    left: 20px;
-    .title {
-      font-size: 36px;
-      font-weight: 500;
-    }
-
-    .search {
-      .material-icons-outlined {
-        display: block;
-        font-size: 36px;
-        position: absolute;
-        right: 90px;
-        
-        &:hover {
-          background-color: #F3F3F3;
-          border-radius: 100%;
-          cursor: pointer;
-        }
-      }
-    }
-    .friend-add {
-      .material-icons-outlined {
-        display: block;
-        font-size: 36px;
-        position: absolute;
-        right: 40px;
-        
-        &:hover {
-          background-color: #F3F3F3;
-          border-radius: 100%;
-          cursor: pointer;
-        }
-      }
-    }
-  }
-  .user__profile {
-    padding: 10px 20px;
-    .profile {
+    flex-direction: column;
+    margin-top: 40px;
+    padding-left: 80px; /** Navigtion width에 따라 변경해야함 */
+    width: 100%;
+    .header {
       display: flex;
-      align-items: center;
-      .profile__image{
-        img {
-          width: 90px;
-          height: 90px;
-          border: 1 solid;
-          border-radius: 30%;
-        }
-      }
-      .profile__textarea{
-        display: flex;
-        flex-direction: column;
-        margin-left: 10px;
-        .profile__name {
-          font-size: 13px;
-          font-weight: 700;
-          margin: 2px 0;
-        }
-        .profile__hello {
-          font-size: 12px;
-          font-weight: 400;
-          margin: 2px 0;
-        }
-      }
-    }
-  }
-  .friends {
-    .friends__title {
       position: relative;
       left: 20px;
-      font-size: 11px;
-    }
+      .title {
+        font-size: 36px;
+        font-weight: 500;
+      }
 
+      .search {
+        .material-icons-outlined {
+          display: block;
+          font-size: 36px;
+          position: absolute;
+          right: 90px;
+          
+          &:hover {
+            background-color: #F3F3F3;
+            border-radius: 100%;
+            cursor: pointer;
+          }
+        }
+      }
+      .friend-add {
+        .material-icons-outlined {
+          display: block;
+          font-size: 36px;
+          position: absolute;
+          right: 40px;
+          
+          &:hover {
+            background-color: #F3F3F3;
+            border-radius: 100%;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+    .contents {
+      width: 100%;
+      height: 100%;
+      overflow-y: auto;
+      overflow-x: hidden;
+      .user__profile {
+        padding: 10px 20px;
+        .profile {
+          display: flex;
+          align-items: center;
+          .profile__image{
+            img {
+              width: 90px;
+              height: 90px;
+              border: 1 solid;
+              border-radius: 30%;
+            }
+          }
+          .profile__textarea{
+            display: flex;
+            flex-direction: column;
+            margin-left: 10px;
+            .profile__name {
+              font-size: 13px;
+              font-weight: 700;
+              margin: 2px 0;
+            }
+            .profile__hello {
+              font-size: 12px;
+              font-weight: 400;
+              margin: 2px 0;
+            }
+          }
+        }
+      }
+      .friends {
+        width: 100%;
+        height: 100%;
+        .friends__title {
+          position: relative;
+          left: 20px;
+          font-size: 11px;
+        }
+        .friend__container {
+          height: 100%;
+          .friend {
+
+          }
+        }
+      }
+    }
   }
 }
 </style>
