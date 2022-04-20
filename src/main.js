a28415ab9a0b860925b04b1e0184297a7aa8eb2e
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-04-20 22:33:02 +0900

    [chore] kakaoInit 기능 추가

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-undef */
 import Vue from 'vue'
 import App from './App.vue'
 import { router } from './routes/index.js';
 import { store } from './store/index.js';
 Vue.config.productionTip = false
 
+Kakao.init(process.env.VUE_APP_JS_KEY)
 new Vue({
   render: h => h(App),
   store,
