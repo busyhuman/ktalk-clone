852e4d5f5d0e8430aa55e2cafda20d8aed46deae
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-04-22 18:20:28 +0900

    [chore] gitignore 수정

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,52 +1,30 @@
 <template>
   <div>
-    auth
   </div>
 </template>
 
 <script>
-/* eslint-disable no-undef */
 export default {
   data() {
     return {
-      token: {
+      autoLogin: '',
+      authorize: {
         baseUrl: 'https://kauth.kakao.com',
-        path: '/oauth/token',
-        grant_type: 'authorization_code',
-        redirect_uri: 'https://localhost:8080/auth',
-        code: this.$route.query.code
+        redirect_uri: 'https://localhost:8080/authmore',
+        response_type: 'code',
+        scope: 'friends'
       },
+      authorizeLink: ''
     }
   },
-  created(){
-    this.fnSelectToken();
+  created() {
+    this.fnInit()
   },
   methods: {
-    /**
-     * 토큰 발급받기
-     */
-    fnSelectToken() {
-      let option = {
-        method: "POST",
-        headers: {
-          "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-        },
-      }
-      fetch(this.token.baseUrl + this.token.path
-      + '?grant_type=' + this.token.grant_type
-      + '&client_id=' + process.env.VUE_APP_REST_KEY
-      + '&redirect_uri=' + this.token.redirect_uri
-      + '&code=' + this.token.code
-      , option)
-      .then( (res) => res.json() )
-      .then( (data) => {
-        Kakao.Auth.setAccessToken(data.access_token)
-        localStorage.setItem('access_token', data.access_token)
-        localStorage.setItem('refresh_token', data.refresh_token)
-        localStorage.setItem('scope', data.scope)
-        this.$router.push('friends')
-      })
-    },
+    fnInit() {
+      this.authorizeLink = this.authorize.baseUrl + '/oauth/authorize' + '?' + 'client_id=' + process.env.VUE_APP_REST_KEY + '&' + 'redirect_uri=' + this.authorize.redirect_uri + '&' + 'response_type=' + this.authorize.response_type + '&' + 'scope=' + this.authorize.scope
+      window.location.href = this.authorizeLink
+    }
   }
 }
 </script>
