ef9c850d91865605ed8987414ef0f59fa3edd414
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-04-22 22:04:51 +0900

    [fix] kakao api scope error 수정

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -4,6 +4,7 @@
 </template>
 
 <script>
+/*global Kakao*/
 export default {
   data() {
     return {
@@ -12,9 +13,16 @@ export default {
         baseUrl: 'https://kauth.kakao.com',
         redirect_uri: 'https://localhost:8080/authmore',
         response_type: 'code',
-        scope: 'friends'
+        scope: ['friends']
       },
-      authorizeLink: ''
+      authorizeLink: '',
+      token: {
+        baseUrl: 'https://kauth.kakao.com',
+        path: '/oauth/token',
+        grant_type: 'authorization_code',
+        redirect_uri: 'https://localhost:8080/auth',
+        code: this.$route.query.code
+      },
     }
   },
   created() {
@@ -22,9 +30,31 @@ export default {
   },
   methods: {
     fnInit() {
-      this.authorizeLink = this.authorize.baseUrl + '/oauth/authorize' + '?' + 'client_id=' + process.env.VUE_APP_REST_KEY + '&' + 'redirect_uri=' + this.authorize.redirect_uri + '&' + 'response_type=' + this.authorize.response_type + '&' + 'scope=' + this.authorize.scope
-      window.location.href = this.authorizeLink
-    }
+      this.fnSelectToken();
+    },
+    /**
+     * 토큰 발급받기
+     */
+    fnSelectToken() {
+      let option = {
+        method: "POST",
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
+      }
+      fetch(this.token.baseUrl + this.token.path
+      + '?grant_type=' + this.token.grant_type
+      + '&client_id=' + process.env.VUE_APP_REST_KEY
+      + '&redirect_uri=' + this.token.redirect_uri
+      + '&code=' + this.token.code
+      , option)
+      .then( (res) => res.json() )
+      .then( (data) => {
+        Kakao.Auth.setAccessToken(data.access_token)
+        this.authorizeLink = this.authorize.baseUrl + '/oauth/authorize' + '?' + 'client_id=' + process.env.VUE_APP_REST_KEY + '&' + 'redirect_uri=' + this.authorize.redirect_uri + '&' + 'response_type=' + this.authorize.response_type + '&' + 'scope=' + this.authorize.scope.join() // 친구목록 동적 동의 화면으로 이동
+        window.location.href = this.authorizeLink
+      })
+    },
   }
 }
 </script>
