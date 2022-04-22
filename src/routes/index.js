26d0739773b71f06168848239b76fcf2e97a908f
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-04-22 18:26:39 +0900

    [feat] 동적 카카오 api 동의 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import friends from '@/views/FriendsView';
 import chats from '@/views/ChatView';
 import more from '@/views/MoreView';
 import auth from '@/views/AuthView';
+import authmore from '@/views/AuthMoreView';
 import NotFound from '@/views/NotFound';
 Vue.use(VueRouter);
 
@@ -19,6 +20,11 @@ export const router = new VueRouter({
       name: 'auth',
       path: '/auth',
       component: auth
+    },
+    {
+      name: 'authmore',
+      path: '/authmore',
+      component: authmore
     },
     {
       name: 'signin',
---
+++ src/views/AuthMoreView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+  </div>
+
+</template>
+
+<script>
+/*global Kakao*/
+export default {
+  data() {
+    return {
+      token: {
+        baseUrl: 'https://kauth.kakao.com',
+        path: '/oauth/token',
+        grant_type: 'authorization_code',
+        redirect_uri: 'https://localhost:8080/authmore',
+        code: this.$route.query.code
+      },
+    }
+  },
+  created(){
+    this.fnSelectToken();
+  },
+  methods: {
+    /**
+     * 토큰 발급받기
+     */
+    fnSelectToken() {
+      let option = {
+        method: "POST",
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
+      }
+      fetch(this.token.baseUrl + this.token.path
+      + '?grant_type=' + this.token.grant_type
+      + '&client_id=' + process.env.VUE_APP_REST_KEY
+      + '&redirect_uri=' + this.token.redirect_uri
+      + '&code=' + this.token.code
+      , option)
+      .then( (res) => res.json() )
+      .then( (data) => {
+        Kakao.Auth.setAccessToken(data.access_token)
+        localStorage.setItem('access_token', data.access_token)
+        localStorage.setItem('refresh_token', data.refresh_token)
+        localStorage.setItem('scope', data.scope)
+        this.$router.push('friends')
+      })
+    },
+  }
+}
+</script>
