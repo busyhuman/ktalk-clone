6e871bb0be70e102283b204e075f424c7f24388b
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-04-26 20:47:51 +0900

    [chore] history 모드로 변경

--- src/App.vue
+++ src/App.vue
@@ -15,20 +15,20 @@ export default {
     }
   },
   created() {
-    this.tokenFreePathList.forEach( (item) => {
-      let path = location.href
-      const idx = path.indexOf(item)
-      console.log(path, idx, location.href )
-      if( idx !== -1 && idx !== 0){
-        if(path[idx-1] !== '#'){
-          let p = ''
-          p += path.substr(0,idx)
-          p += '/#'
-          p += path.substr(idx, path.length);
-          location.href = p
-        }
-      }
-    })
+    // this.tokenFreePathList.forEach( (item) => {
+    //   let path = location.href
+    //   const idx = path.indexOf(item)
+    //   console.log(path, idx, location.href )
+    //   if( idx !== -1 && idx !== 0){
+    //     if(path[idx-1] !== '#'){
+    //       let p = ''
+    //       p += path.substr(0,idx)
+    //       p += '/#'
+    //       p += path.substr(idx, path.length);
+    //       location.href = p
+    //     }
+    //   }
+    // })
     if(this.tokenFreePathList.filter( item => item === this.$route.path).length === 0 ){  // token이 필요한 path라면
       if (!Kakao.Auth.getAccessToken()) { // 토큰이 없다면
           this.$router.push('signin')
