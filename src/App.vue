d84c986e7be4845ea90dc9f791d4ce93dbeaf769
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-04-26 19:32:17 +0900

    [chore] hash모드 변경에 따른 redirect_uri에 # 추가

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,24 @@ export default {
   name: 'App',
   data() {
     return {
-      tokenFreePathList: ['/signin', '/auth', '/authmore'],
+      tokenFreePathList: ['/auth', '/authmore', '/signin'],
     }
   },
   created() {
+    this.tokenFreePathList.forEach( (item) => {
+      let path = location.href
+      const idx = path.indexOf(item)
+      console.log(path, idx, location.href )
+      if( idx !== -1 && idx !== 0){
+        if(path[idx-1] !== '#'){
+          let p = ''
+          p += path.substr(0,idx)
+          p += '/#'
+          p += path.substr(idx, path.length);
+          location.href = p
+        }
+      }
+    })
     if(this.tokenFreePathList.filter( item => item === this.$route.path).length === 0 ){  // token이 필요한 path라면
       if (!Kakao.Auth.getAccessToken()) { // 토큰이 없다면
           this.$router.push('signin')
