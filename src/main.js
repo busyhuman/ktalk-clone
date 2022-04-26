f7c6dc870f63d20bab61abcb5971fa79ddf502ab
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-04-26 23:06:34 +0900

    build

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import App from './App.vue'
 import { router } from './routes/index.js';
 import { store } from './store/index.js';
 Vue.config.productionTip = false
-console.log(typeof(process.env.VUE_APP_JS_KEY))
-Kakao.init('2495bf6ebc1031589125c6fc21f9294b')
+
+Kakao.init(process.env.VUE_APP_JS_KEY)
 new Vue({
   render: h => h(App),
   store,
