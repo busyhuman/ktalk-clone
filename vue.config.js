4592d9d12b2003e48303d30103484ff1812b02fc
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-05-06 19:26:36 +0900

    [chore] entry 수정

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: '/',
   outputDir: './docs',
   devServer: {
     https: {
