7f70a0dd25250c05d01d21350df6fbe2b975c860
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-05-08 18:25:43 +0900

    [feat] 테스트 코드 추가

---
+++ src/utils/__test__/views.test.js
@@ -0,0 +1,24 @@
+import * as utils from '../util'
+describe('utils 테스트', () => {
+  test('isNull 테스트1', () => {
+    expect(utils.isNull('a')).toBe(false)
+  })
+  test('isNull 테스트2', () => {
+    expect(utils.isNull('1')).toBe(false)
+  })
+  test('isNull 테스트3', () => {
+    expect(utils.isNull('[]')).toBe(false)
+  })
+  test('isNull 테스트4', () => {
+    expect(utils.isNull(undefined)).toBe(true)
+  })
+  test('isNull 테스트5', () => {
+    expect(utils.isNull('')).toBe(true)
+  })
+  test('isNull 테스트6', () => {
+    expect(utils.isNull(null)).toBe(true)
+  })
+  test('isNull 테스트7', () => {
+    expect(utils.isNull('@')).toBe(false)
+  })
+})
