c0a805d2aa56517556c09dffa74678489c785392
DESKTOP-1F1B866\busyh <busyhuman@naver.com>
2022-05-08 19:19:12 +0900

    [chore] 테스트코드 수정

--- src/store/__test__/store.test.js
+++ src/store/__test__/store.test.js
@@ -2,12 +2,12 @@
 import { store } from '../index'
 describe('store 테스트', () => {
   test('alarm 테스트', () => {
-    expect(store.state.toggle.alarm).toBe(true)
+    expect(store.state.toggle.alarm).toBeDefined()
   })
   test('setting 테스트', () => {
-    expect(store.state.toggle.setting).toBe(false)
+    expect(store.state.toggle.setting).toBeDefined()
   })
   test('chatSort 테스트', () => {
-    expect(store.state.toggle.chatSort).toBe(false)
+    expect(store.state.toggle.chatSort).toBeDefined()
   })
 })
--- src/utils/__test__/utils.test.js
+++ src/utils/__test__/utils.test.js
@@ -2,24 +2,24 @@
 import * as utils from '../util'
 describe('utils 테스트', () => {
   test('isNull 테스트1', () => {
-    expect(utils.isNull('a')).toBe(false)
+    expect(utils.isNull('a')).toBeFalsy()
   })
   test('isNull 테스트2', () => {
-    expect(utils.isNull('1')).toBe(false)
+    expect(utils.isNull('1')).toBeFalsy()
   })
   test('isNull 테스트3', () => {
-    expect(utils.isNull('[]')).toBe(false)
+    expect(utils.isNull('[]')).toBeFalsy()
   })
   test('isNull 테스트4', () => {
-    expect(utils.isNull(undefined)).toBe(true)
+    expect(utils.isNull(undefined)).toBeTruthy()
   })
   test('isNull 테스트5', () => {
-    expect(utils.isNull('')).toBe(true)
+    expect(utils.isNull('')).toBeTruthy()
   })
   test('isNull 테스트6', () => {
-    expect(utils.isNull(null)).toBe(true)
+    expect(utils.isNull(null)).toBeTruthy()
   })
   test('isNull 테스트7', () => {
-    expect(utils.isNull('@')).toBe(false)
+    expect(utils.isNull('@')).toBeFalsy()
   })
 })
